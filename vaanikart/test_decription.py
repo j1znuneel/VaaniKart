5c9ddce393f9d093f1e3971e63c673a404653175
abhinand128 <abhinanda128@gmail.com>
2025-07-15 17:57:06 +0530

    product_description_generatation_added

--- vaanikart/test_decription.py
+++ vaanikart/test_decription.py
@@ -1,4 +1,4 @@
-from gemini_description import generate_product_description_groq
+from groq_description import generate_product_description_groq
 
 
 text = "ചക്ക ചിപ്‌സ്, വീട്ടിൽ ഉണ്ടാക്കിയത്, 250 ഗ്രാം, 80 രൂപ"
