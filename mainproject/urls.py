a304a659189baa4db7d7efad332f545f1d4d565e
Tharun <tharunraman10@gmail.com>
2025-07-15 15:51:55 +0530

    webhook url added

--- mainproject/urls.py
+++ mainproject/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from vaanikart.views import whatsapp_webhook
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("webhook/", whatsapp_webhook, name="whatsapp_webhook"),
+
 ]
